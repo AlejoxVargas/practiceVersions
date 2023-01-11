7f219e4b370f8e9a655b9d974e9bd611068a6afc
Alejandro Vargas <114786397+AlejoxVargas@users.noreply.github.com>
2023-01-11 13:11:11 +0100

    Add files via upload
    New files updated

--- Retos4_1/src/main/java/MatricesUnidimensionales/Ejercicio3.java
+++ Retos4_1/src/main/java/MatricesUnidimensionales/Ejercicio3.java
@@ -1,11 +1,27 @@
 package MatricesUnidimensionales;
 
+import java.util.Scanner;
+
+/*Escribir un método alternar() al que se le pase como argumento el array números y dos índices del array. 
+El método alternará los valores contenidos en los dos índices. Se deberá comprobar que los índices están dentro del rango del array.*/
 public class Ejercicio3 {
+
     public static void main(String[] args) {
-        int posicion1=0, posicion2=0;
+        Scanner sc = new Scanner(System.in);
         int[] getArray = {12, 15, 13, 10, 8, 9, 13, 14};
+        System.out.println("Escribir primer índice: ");
+        int posicion1 = sc.nextInt();
+        System.out.println("Escribir segundo índice: ");
+        int posicion2 = sc.nextInt();
+        alternar(getArray,posicion1, posicion2);
     }
-    static boolean alternar (int[]getArray, int posicion1, int posicion2){
-    
+
+    static boolean alternar(int[] getArray, int posicion1, int posicion2) {
+        boolean llave = false;
+        for (int i : getArray) {
+            if(getArray[i]!=posicion1){
+               
+            }
+        }
     }
 }
