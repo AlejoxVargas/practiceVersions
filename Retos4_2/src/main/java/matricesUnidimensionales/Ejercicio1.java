84908e8e43914d0853d80f6f1dc0f94cfee48ea2
Alejandro Vargas <114786397+AlejoxVargas@users.noreply.github.com>
2023-01-13 12:06:39 +0100

    Add files via upload
    Inicio de Retos 4.2, estoy empezando el ejercicio 1 pero tengo problemas para imprimir la cadena de caracteres ingresados previamente.

---
+++ Retos4_2/src/main/java/matricesUnidimensionales/Ejercicio1.java
@@ -0,0 +1,39 @@
+package matricesUnidimensionales;
+
+import java.util.Scanner;
+
+/*1. Construir un array de caracteres que contenga las letras de la ‘A’ a la ‘Z’ (solo las mayúsculas sin tener en cuenta las letras 'Ñ', 'LL'...). 
+Posteriormente se pedirán posiciones del array por teclado y, si la posición es correcta, se añadirá el carácter correspondiente 
+a una cadena que se imprimirá al final, se dejará de insertar cuando se introduzca un número negativo.
+
+Por ejemplo, si se introducen los siguientes números:
+0 // Se añade la ‘A’
+6 // Se añade la ‘G’
+25 // Se añade la ‘Z’
+50 // Insertar números entre 0 y 25
+-3 // Fin
+
+Cadena final: AGZ*/
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] charArray = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        int posicion;
+        boolean llave = false;
+        do {
+            System.out.println("Enter the array position: ");
+            posicion = sc.nextInt();
+            
+            if (posicion > charArray.length) {
+                System.out.println("Insertar números entre 0 y 25");
+            } else if (posicion < 0) {
+                llave = true;
+            } else {
+                System.out.println("Se añade la: " + "\'" + charArray[posicion] + "\'");
+            }
+        } while (!llave);
+        System.out.println("Fin de la ejecución");
+        //System.out.println("Cadena final:" + charArray[posicion]);
+    }
+}
