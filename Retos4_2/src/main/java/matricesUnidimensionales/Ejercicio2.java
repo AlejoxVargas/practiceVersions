d9fa85a403761e7db96c083bdb1d138bdb9be189
Alejandro Vargas <114786397+AlejoxVargas@users.noreply.github.com>
2023-01-16 20:26:07 +0100

    Add files via upload
    Retos 4.2 en el ejercicio 2 he tenido alguna ayuda del profesor, pero Mishel me ha ayudado a ver la solución, me falta aún el 5 con problemas en los decimales.

--- Retos4_2/src/main/java/matricesUnidimensionales/Ejercicio2.java
+++ Retos4_2/src/main/java/matricesUnidimensionales/Ejercicio2.java
@@ -7,33 +7,33 @@ que en la tabla ASCII la letra 'A' corresponde al número 65 y la letra 'Z' corr
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner("System.in");
+        Scanner sc = new Scanner(System.in);
         char[] charArray = new char[25];
         int posicion;
         boolean llave = false;
         int ascii = 65;
         String acumulador = "";
+        
         do {
-            System.out.println("Enter the array position: ");
+            System.out.println("Inserte indice: ");
             posicion = sc.nextInt();
-
+            
             if (posicion > charArray.length) {
                 System.out.println("Insertar números entre 0 y 25");
             } else if (posicion < 0) {
                 llave = true;
             } else {
-                for (int i = 0; i < charArray.length; i++) {
-                    System.out.println(ascii);
-                    ascii+=1;
-                }
-                System.out.println("Se añade la: " + "\'" + charArray[posicion] + "\'");
+                for (int i = 0; i <= charArray.length; i++) {
+                    ascii += posicion;
+                    System.out.println((char)ascii);
+                //System.out.println("Se añade la: " + "\'" + charArray[i] + "\'");
                 acumulador += charArray[posicion];
+                }
             }
 
         } while (!llave);
-        System.out.println("Fin de la ejecución");
-        System.out.println("Cadena final: " + acumulador);
+         System.out.println("Fin de la ejecución");
+         System.out.println("Cadena final: " + acumulador);
 
     }
 }
-//ERRORES
