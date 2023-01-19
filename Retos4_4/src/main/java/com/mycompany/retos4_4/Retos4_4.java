ad28eaad4e9f29dc44748982fbccbd5583d5d5b0
Alejandro Vargas <114786397+AlejoxVargas@users.noreply.github.com>
2023-01-19 20:33:03 +0100

    Add files via upload
    Avance reto 4.4

---
+++ Retos4_4/src/main/java/com/mycompany/retos4_4/Retos4_4.java
@@ -0,0 +1,26 @@
+package com.mycompany.retos4_4;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Retos4_4 {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rnd = new Random();
+
+    public static void main(String[] args) {
+        final int CANTIDAD_NUMEROS = 6;
+        int[] ganadora = new int[CANTIDAD_NUMEROS];
+        int[] miPrimitiva = new int[CANTIDAD_NUMEROS];
+        
+        //Cargar Array ganadora
+        for (int i = 0; i < ganadora.length; i++){
+            System.out.println(ganadora);
+        }
+        //Cargar Array miPrimitiva
+        for (int i = 0; i < ganadora.length; i++){
+            
+        }       
+    }
+}
