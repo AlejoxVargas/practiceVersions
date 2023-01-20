ff4e129bb88903151e80f02589737812d4743f87
Alejandro Vargas <114786397+AlejoxVargas@users.noreply.github.com>
2023-01-20 11:38:57 +0100

    Add files via upload
    Reto 4.4 de lotería primitiva, culminado, esta por presentar a Javier y revisar

--- Retos4_4/src/main/java/com/mycompany/retos4_4/Retos4_4.java
+++ Retos4_4/src/main/java/com/mycompany/retos4_4/Retos4_4.java
@@ -13,14 +13,70 @@ public class Retos4_4 {
         final int CANTIDAD_NUMEROS = 6;
         int[] ganadora = new int[CANTIDAD_NUMEROS];
         int[] miPrimitiva = new int[CANTIDAD_NUMEROS];
-        
-        //Cargar Array ganadora
-        for (int i = 0; i < ganadora.length; i++){
-            System.out.println(ganadora);
+
+        inicializarArrayAleatorio(ganadora);
+        inicializarArrayUsuario(miPrimitiva);
+
+        System.out.println("La combinación ganadora es  : " + Arrays.toString(ganadora));
+        System.out.println("Tu apuesta es : " + Arrays.toString(miPrimitiva));
+        System.out.println("Has tenido " + comprobarAciertos(ganadora, miPrimitiva) + " acierto(s)");
+    }
+
+    //metodos 
+    static void inicializarArrayAleatorio(int[] vector) {
+        boolean descartado;
+        for (int i = 0; i < vector.length; i++) {
+            do {
+                int numeroRandom = rnd.nextInt(50);
+                descartado = false;
+                for (int numVector : vector) {
+                    if (numVector == numeroRandom) {
+                        descartado = true;
+                        break;
+                    }
+                }
+                if (!descartado) {
+                    vector[i] = numeroRandom;
+                }
+            } while (descartado);
         }
-        //Cargar Array miPrimitiva
-        for (int i = 0; i < ganadora.length; i++){
-            
-        }       
+    }
+
+    static void inicializarArrayUsuario(int[] vector) {
+        int count = 1;
+        boolean llave;
+        for (int i = 0; i < vector.length; i++) {
+            do {
+                System.out.println("Ingrese " + count + " miPrimitiva: ");
+                int numero = sc.nextInt();
+                llave = false;
+                if (numero < 1 || numero > 49) {
+                    System.out.println("Numero fuera del rango 1-49, intentelo de nuevo");
+                    llave = true;
+                }
+                for (int numVector : vector) {
+                    if (numVector == numero) {
+                        System.out.println("Número ya se encuentra");
+                        llave = true;
+                    }
+                }
+                if (!llave) {
+                    vector[i] = numero;
+                }
+            } while (llave);
+            count++;
+        }
+    }
+
+    static int comprobarAciertos(int[] vector1, int[] vector2) {
+        int aciertos = 0;
+        for (int i = 0; i < vector1.length; i++) {
+            for (int j = 0; j < vector2.length; j++) {
+                if (vector1[i] == vector2[j]){
+                aciertos ++;
+                }
+            }
+        }
+        return aciertos;
     }
 }
