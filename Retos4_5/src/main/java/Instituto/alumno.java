99bacfc3695ce0408c6e20d5be9bc1b580f38a16
Alejandro Vargas <114786397+AlejoxVargas@users.noreply.github.com>
2023-01-20 20:35:25 +0100

    Add files via upload
    ejercicios 4.5 en proceso tengo algunos problemas con asignar el array grupos

--- Retos4_5/src/main/java/Instituto/alumno.java
+++ Retos4_5/src/main/java/Instituto/alumno.java
@@ -1,17 +1,14 @@
 package Instituto;
 
 import java.time.LocalDate;
-import java.util.Scanner;
 
 public class alumno {
 
-    static Scanner sc = new Scanner(System.in);
-
-    //Atriu¡butos 
+    //Atributos 
     String nombre;
     String apellido;
     LocalDate fechaNacimiento;
-    double[] notas = new double[3];
+    static double[] notas = new double[3];
 
     //Constructor 
     public alumno(String nombre, String apellido, LocalDate fechaNacimiento) {
@@ -24,59 +21,51 @@ public class alumno {
     }
 
     //Metodos
-    static void setNotaIngles(alumno alumnoX) {
-        System.out.println("Nota Inglés: ");
-        int notaIngles = sc.nextInt();
-        
+    static void setNotaIngles(alumno alumnoX, double notaIngles) {
         if (notaIngles < 0 || notaIngles > 10) {
             System.out.println("Calificación incorrecta");
         } else {
-            alumnoX.notaIngles = notaIngles;
+            notas[0] = notaIngles;
+            //alumnoX.notas[0] = notaIngles;
             System.out.println("Exitosa asignación");
             System.out.println(alumnoX.toString());
         }
     }
 
-    static void setNotaFrances(alumno alumnoX) {
-        System.out.println("Nota francés: ");
-        int notaFrances = sc.nextInt();
-
+    static void setNotaFrances(alumno alumnoX, double notaFrances) {
         if (notaFrances < 0 || notaFrances > 10) {
             System.out.println("Calificación incorrecta");
         } else {
-            alumnoX.notaFrances = notaFrances;
+            notas[1] = notaFrances;
             System.out.println("Exitosa asignación");
             System.out.println(alumnoX.toString());
         }
     }
 
-    static void setNotaAleman(alumno alumnoX) {
-        System.out.println("Nota Alemán: ");
-        int notaAleman = sc.nextInt();
-        
+    static void setNotaAleman(alumno alumnoX, double notaAleman) {
         if (notaAleman < 0 || notaAleman > 10) {
             System.out.println("Calificación incorrecta");
         } else {
-            alumnoX.notaAleman = notaAleman;
+            notas[2] = notaAleman;
             System.out.println("Exitosa asignación");
             System.out.println(alumnoX.toString());
         }
     }
 
     //Calcular Media
-    static void calcularMedia(alumno alumX) {
-        double resultado = (alumX.notaIngles + alumX.notaFrances + alumX.notaAleman) / 3;
+    static void calcularMedia(alumno alumnoX) {
+        double resultado = (alumnoX.notas[0] + alumnoX.notas[1] + alumnoX.notas[2]) / 3;
         System.out.println("Nota media: " + resultado);
     }
 
-    //ToString 
     @Override
     public String toString() {
-        return "\tResumen del estudiante"
-                + "\nNombre del estudiante: " + nombre
-                + "\nIngles:" + notaIngles
-                + "\nFrancés:" + notaFrances
-                + "\nAlemán:" + notaAleman;
-
+        return "\nNombre del estudiante: " + nombre
+                + "\nApellido:" + apellido
+                + "\nFecha nacimiento:" + fechaNacimiento
+                + "\nIngles:" + notas[0]
+                + "\nFrancés:" + notas[1]
+                + "\nAlemán:" + notas[2]
+                + "\n";
     }
 }
--- Retos4_5/src/main/java/Instituto/principal.java
+++ Retos4_5/src/main/java/Instituto/principal.java
@@ -1,16 +1,92 @@
 package Instituto;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class principal {
 
+    static Scanner sc = new Scanner(System.in);
+    
+    static alumno[] grupo = new alumno[6];
+    static alumno alumno1 = new alumno("Jennie", "Kim", LocalDate.of(1996, 1, 16));
+    static alumno alumno2 = new alumno("Ji-soo", "Kim", LocalDate.of(1995, 1, 3));
+    static alumno alumno3 = new alumno("Lalisa", "Manobal", LocalDate.of(1997, 3, 27));
+    static alumno alumno4 = new alumno("Roseanne", "Park", LocalDate.of(1997, 2, 11));
+    static alumno alumno5 = new alumno("Momo", "Hirai", LocalDate.of(1996, 11, 9));
+    static alumno alumnoAux = new alumno();
+
     public static void main(String[] args) {
-        alumno alumno1 = new alumno("Jennie", "Kim", LocalDate.of(1996, 1, 16));
-        alumno alumno2 = new alumno("Ji-soo", "Kim", LocalDate.of(1995, 1, 3));
-        alumno alumno3 = new alumno("Lalisa", "Manobal", LocalDate.of(1997, 3, 27));
-        alumno alumno4 = new alumno("Roseanne", "Park", LocalDate.of(1997, 2, 11));
-        alumno alumno5 = new alumno("Momo", "Hirai", LocalDate.of(1996, 11, 9));
+
+        int opcion;
+        do {
+            opcion = menu();
+            if (opcion != 0) {
+                alumnoAux = elegirAlumno();
+            }
+            switch (opcion) {
+                case 1 -> {
+                    opcion = elegirAsignatura();
+                    switch (opcion) {
+                        case 1 ->
+                            alumno.setNotaIngles(alumnoAux, elegirNota());
+                        case 2 ->
+                            alumno.setNotaFrances(alumnoAux, elegirNota());
+                        case 3 ->
+                            alumno.setNotaAleman(alumnoAux, elegirNota());
+                    }
+                }
+                case 2 -> {
+                    alumno.calcularMedia(alumnoAux);
+                }
+                default ->
+                    System.out.println("Fin");
+            }
+        } while (opcion != 0);
 
     }
 
+    static int menu() {
+        int opcion;
+        do {
+            System.out.println(
+                    "\tMENÚ INSTITUTO"
+                    + "\n----------------------------"
+                    + "\n 1.-Asignar Nota"
+                    + "\n 2.-Calcular Media"
+                    + "\n 0.-Salir\n");
+            System.out.println(" Seleccione opción: ");
+            opcion = sc.nextInt();
+        } while (opcion < 0 || opcion > 2);
+        return opcion;
+    }
+
+    static alumno elegirAlumno() {
+        System.out.println("Elegir estudiante: ");
+        switch (sc.nextInt()) {
+            case 1 ->
+                alumnoAux = alumno1;
+            case 2 ->
+                alumnoAux = alumno2;
+            case 3 ->
+                alumnoAux = alumno3;
+            case 4 ->
+                alumnoAux = alumno4;
+            case 5 ->
+                alumnoAux = alumno5;
+        }
+        return alumnoAux;
+    }
+
+    static int elegirAsignatura() {
+        System.out.println("1.-Inglés\n2.-Francés\n3.-Aleman\n");
+        System.out.println("Seleccione opción: ");
+        int opcion = sc.nextInt();
+        return opcion;
+    }
+
+    static double elegirNota() {
+        System.out.println("Digita número entre 0-10 (con o sin decimales)");
+        double opcion = sc.nextDouble();
+        return opcion;
+    }
 }
