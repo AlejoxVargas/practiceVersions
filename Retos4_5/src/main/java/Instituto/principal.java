d466eb1e207c8e8d2cc5a9dedddceb1a21b777a4
Alejandro Vargas <114786397+AlejoxVargas@users.noreply.github.com>
2023-01-23 12:53:42 +0100

    Add files via upload
    Reto 4.5 he tratado de buscar algunas soluciones para como crear el grupo con varios objetos, pero aún no he avanzado del todo, quedo pendiente a hablar con el profesor y revisar que sucede

--- Retos4_5/src/main/java/Instituto/principal.java
+++ Retos4_5/src/main/java/Instituto/principal.java
@@ -6,22 +6,19 @@ import java.util.Scanner;
 public class principal {
 
     static Scanner sc = new Scanner(System.in);
-    
-    static alumno[] grupo = new alumno[6];
-    static alumno alumno1 = new alumno("Jennie", "Kim", LocalDate.of(1996, 1, 16));
-    static alumno alumno2 = new alumno("Ji-soo", "Kim", LocalDate.of(1995, 1, 3));
-    static alumno alumno3 = new alumno("Lalisa", "Manobal", LocalDate.of(1997, 3, 27));
-    static alumno alumno4 = new alumno("Roseanne", "Park", LocalDate.of(1997, 2, 11));
-    static alumno alumno5 = new alumno("Momo", "Hirai", LocalDate.of(1996, 11, 9));
     static alumno alumnoAux = new alumno();
 
     public static void main(String[] args) {
 
+//        alumno grupo[] = new alumno[6];
+//        for (int i = 0; i < grupo.length; i++) {
+//            grupo[i] = new alumno(crearNombre(), crearApellido(), LocalDate.of(crearAño(), crearMes(), crearDia()));
+//        }
         int opcion;
         do {
             opcion = menu();
             if (opcion != 0) {
-                alumnoAux = elegirAlumno();
+                //alumnoAux = elegirAlumno();
             }
             switch (opcion) {
                 case 1 -> {
@@ -38,10 +35,19 @@ public class principal {
                 case 2 -> {
                     alumno.calcularMedia(alumnoAux);
                 }
+                case 3 -> {
+//                    int cantidad = 1;
+//                    alumno grupo[] = new alumno[6];
+//                    for (int i = 0; i < cantidad; i++) {
+//                        grupo[i] = new alumno(crearNombre(), crearApellido(), LocalDate.of(crearAño(), crearMes(), crearDia()));
+//                    }
+//                    System.out.println("¿Quiere registrar a otro estudiante?");
+                }
                 default ->
                     System.out.println("Fin");
             }
-        } while (opcion != 0);
+        } while (opcion
+                != 0);
 
     }
 
@@ -53,6 +59,7 @@ public class principal {
                     + "\n----------------------------"
                     + "\n 1.-Asignar Nota"
                     + "\n 2.-Calcular Media"
+                    + "\n 3.-Registrar Alumno"
                     + "\n 0.-Salir\n");
             System.out.println(" Seleccione opción: ");
             opcion = sc.nextInt();
@@ -60,11 +67,11 @@ public class principal {
         return opcion;
     }
 
-    static alumno elegirAlumno() {
+    static alumno elegirAlumno(alumno alumnoX) {
         System.out.println("Elegir estudiante: ");
         switch (sc.nextInt()) {
             case 1 ->
-                alumnoAux = alumno1;
+                alumnoAux = alumnoX;
             case 2 ->
                 alumnoAux = alumno2;
             case 3 ->
@@ -89,4 +96,57 @@ public class principal {
         double opcion = sc.nextDouble();
         return opcion;
     }
+
+    static String crearNombre() {
+        System.out.println("Nombre: ");
+        String nombre = sc.nextLine();
+        return nombre;
+    }
+
+    static String crearApellido() {
+        System.out.println("Apellido:");
+        String apellido = sc.nextLine();
+        return apellido;
+    }
+
+    static int crearAño() {
+
+        System.out.println("Año: ");
+        int año = sc.nextInt();
+        return año;
+    }
+
+    static int crearMes() {
+        System.out.println("Mes: ");
+        int mes = sc.nextInt();
+        return mes;
+    }
+
+    static int crearDia() {
+        System.out.println("Día: ");
+        int dia = sc.nextInt();
+        return dia;
+    }
+
+    static alumno registrarAlumno() {
+        boolean llave = true;
+        int cantidad = 1;
+        alumno grupo[] = new alumno[6];
+        do {
+            for (int i = 0; i < cantidad; i++) {
+                grupo[i] = new alumno(crearNombre(), crearApellido(), LocalDate.of(crearAño(), crearMes(), crearDia()));
+            }
+            System.out.println(
+                    "¿Quiere registrar a otro estudiante?"
+                    + "\n1.Si"
+                    + "\n2.No");
+            int opcion = sc.nextInt();
+            if (opcion == 1) {
+                llave = true;
+                cantidad++;
+            }
+        } while (llave);
+        return alumno;
+    }
 }
+
