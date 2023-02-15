b2ad3c6bd39b38782df87bf0f80a263faa4ea2f0
Alejo CCC <henrryalejandro21@gmail.com>
2023-02-15 20:44:40 +0100

    commit

--- Base Datos/Triggers/Triggers1.sql
+++ Base Datos/Triggers/Triggers1.sql
@@ -82,3 +82,60 @@ INSERT INTO leads (first_name,last_name,email,phone,source)
 VALUES('Pepe','Pepito','pepe@pepito.pepecom','114567898','pepe');
 
 UPDATE leads SET phone = '123456789' WHERE id = 2;
+
+--------------------------------------------
+CREATE TRIGGER log_contact_after_insert
+	AFTER INSERT ON leads
+	BEGIN
+INSERT INTO lead_logs (
+	old_id,
+	new_id,
+	old_phone,
+	new_phone,
+	old_email,
+	new_email,
+	user_action,
+	created_at
+)
+VALUES
+(
+	" ",
+	new.id,
+	' ',
+	new.phone,
+	' ',
+	new.email,
+	'INSERT',
+	DATETIME('NOW')
+) ;
+END;
+INSERT INTO leads (first_name,last_name,email,phone,source)
+VALUES('ROJAS','MARTINEZ','rojas@martinez.com','111111111','rojas');
+-------------------------------------
+CREATE TRIGGER log_contact_after_delete
+	AFTER DELETE ON leads
+	BEGIN
+INSERT INTO lead_logs (
+	old_id,
+	new_id,
+	old_phone,
+	new_phone,
+	old_email,
+	new_email,
+	user_action,
+	created_at
+)
+VALUES
+(
+	old.id,
+	'',
+	old.phone,
+	'',
+	old.email,
+	'',
+	'DELETE',
+	DATETIME('NOW')
+) ;
+END;
+
+DELETE FROM leads WHERE id = 5;
