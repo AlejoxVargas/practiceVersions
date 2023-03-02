d30780462a590ab533ad40751171efabad4fbd13
AlejoxVargas <henrryalejandro21@gmail.com>
2023-03-02 12:38:04 +0100

    Bootstrap ejercicio5

--- EclipseWorkSpace/Retos5_2/src/empresa/Jefes.java
+++ EclipseWorkSpace/Retos5_2/src/empresa/Jefes.java
@@ -1,27 +1,27 @@
 package empresa;
 
 public class Jefes extends Empleado {
-	private double prima;
+    private double prima;
 
-	public Jefes(String nombre, double sueldo, double prima) {
-		super(nombre, sueldo);
-		this.prima = prima;
-	}
+    public Jefes(String nombre, double sueldo, double prima) {
+        super(nombre, sueldo);
+        this.prima = prima;
+    }
 
-	public double getPrima() {
-		return prima;
-	}
+    public double getPrima() {
+        return prima;
+    }
 
-	public void setPrima(double prima) {
-		this.prima += super.getSueldo() + prima;
-	}
+    public void setPrima(double prima) {
+        this.prima = prima;
+    }
 
-	public double getSueldo() {
-		return super.getSueldo();
-	}
+    public double getSueldo() {
+        return super.getSueldo() + this.prima;
+    }
 
-	@Override
-	public String toString() {
-		return super.toString() + "Prima: " + getPrima() + "\n";
-	}
+    @Override
+    public String toString() {
+        return super.toString() + "Prima: " + getPrima() + "\nSueldo + Prima: " + this.getSueldo() + "\n";
+    }
 }
