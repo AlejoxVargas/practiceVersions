dadb37f0d19d65517f357d167722383de4f5cc0c
AlejoxVargas <henrryalejandro21@gmail.com>
2023-03-06 13:36:24 +0100

    avance 6.1

--- EclipseWorkSpace/Retos6_1/src/Gestion/PrincipalGestion.java
+++ EclipseWorkSpace/Retos6_1/src/Gestion/PrincipalGestion.java
@@ -1,71 +1,117 @@
 package Gestion;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PrincipalGestion {
-	static Scanner sc = new Scanner (System.in);
-	public static void main(String[] args) {
-		Empleado[] plantilla = new Empleado[10];
-	}
-	
-	public static int opcionMenu(){
-		int menu = 0;
-		boolean valido = false;
-		
-		do {
-			try {
-				System.out.println("\n _____MENU_____"
-						+"\n1. Agregar Empleado"
-						+"\n2. Consulta Sueldo de empleado"
-						+"\n3. Consulta Sueldo Medio por Categorias"
-						+"\n4. Consulta de Categorias segun criterios"
-						+"\n0. Salir");
-				menu = sc.nextInt();
-				valido = true;
-			} catch(InputMismatchException e) {
-				System.out.println("Tiene que ser un número entero entre 1 y 4");
-			}
-		}while(!valido);
-		
-		return menu;
-	}
-	
-	public static void crear(Empleado[] plantilla, int opcion) {
-		String nombre;
-		String apellido;
-		int edad;
-		int año,mes,dia;
+    static Scanner sc = new Scanner(System.in);
 
-		sc.nextLine();
+    public static void main(String[] args) {
+        Menu();
+    }
 
-		System.out.println("Nombre: ");
-		nombre = sc.nextLine();
-		System.out.println("Apellido: ");
-		apellido = sc.nextLine();
-		System.out.println("edad: ");
-		edad = sc.nextInt();
-		
+    public static void Menu() {
+        int menu;
+        boolean llave = true;
+        do {
+            menu = opcionMenu();
+            switch (menu){
+                case 1 -> crearEmpleado(crearArray(),crearTipoEmpleadoMenu());
+            }
+        } while (menu != 0);
+    }
 
-		switch (opcion) {
-		case 1 -> {
-			Empleado
-		}
-		case 2 -> {
-			System.out.println("Prima: ");
-			prima = sc.nextDouble();
-			EmpRepresentante(new Jefes(nombre, sueldo, prima));
-		case 3 -> {
-			sc.nextLine();
-			System.out.println("Contrato: ");
-			contrato = sc.nextLine();
-			EmpVentas(new Becarios(nombre, sueldo, contrato));
-		}
-		}
-		System.out.println(Empresa.toString());
-	}
-	//hacer una clase de localdate para rellenar de manera directa y sin tener 3 variables distintas
-	
-	
+    public static int opcionMenu() {
+        int menu = 0;
+        boolean valido = false;
+        do {
+            try {
+                System.out.println("""
+                         _____MENU_____
+                        1. Agregar Empleado
+                        2. Consulta Sueldo de empleado
+                        3. Consulta Sueldo Medio por Categorias
+                        4. Consulta de Categorias segun criterios
+                        0. Salir""");
+                menu = sc.nextInt();
+                valido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Tiene que ser un número entero entre 1 y 4");
+            }
+        } while (!valido);
+
+        return menu;
+    }
+
+    public static Empleado[] crearArray() {
+        int personalCantidad = 0;
+        System.out.println("Tamaño de la plantila de la empresa: ");
+        personalCantidad = sc.nextInt();
+        Empleado[] plantilla = new Empleado[personalCantidad];
+        return plantilla;
+    }
+
+    public static void crearEmpleado(Empleado[] plantilla, int opcion) {
+        String nombre;
+        String apellido;
+        int edad;
+        int anyo, mes, dia;
+
+        sc.nextLine();
+
+        System.out.println("Nombre: ");
+        nombre = sc.nextLine();
+        System.out.println("Apellido: ");
+        apellido = sc.nextLine();
+        System.out.println("Edad: ");
+        edad = sc.nextInt();
+        System.out.println("Año:");
+        anyo = sc.nextInt();
+        System.out.println("Mes: ");
+        mes = sc.nextInt();
+        System.out.println("Día: ");
+        dia = sc.nextInt();
+
+        for (int i = 0; i < plantilla.length; i++) {
+            switch (opcion) {
+                case 1 -> {
+                    plantilla[i] = new EmpVenta(nombre, apellido, edad, LocalDate.of(anyo, mes, dia));
+                }
+                case 2 -> {
+                    plantilla[i] = new EmpRepresentantes(nombre, apellido, edad, LocalDate.of(anyo, mes, dia));
+                }
+                case 3 -> {
+                    plantilla[i] = new EmpProduccion(nombre, apellido, edad, LocalDate.of(anyo, mes, dia));
+                }
+                case 4 -> {
+                    plantilla[i] = new EmpMantenimiento(nombre, apellido, edad, LocalDate.of(anyo, mes, dia));
+                }
+                case 5 -> opcionMenu();
+            }
+        }
+    }
+
+    public static int crearTipoEmpleadoMenu(){
+        int menu = 0;
+        boolean valido = false;
+        do {
+            try {
+                System.out.println("""
+                         _____EMPLEADO_____
+                        1. Venta
+                        2. Representantes
+                        3. Producción
+                        4. Mantenimiento
+                        5. Menu Principal
+                        0. Salir""");
+                menu = sc.nextInt();
+                valido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Tiene que ser un número entero entre 1 y 4");
+            }
+        } while (!valido);
+
+        return menu;
+    }
 }
