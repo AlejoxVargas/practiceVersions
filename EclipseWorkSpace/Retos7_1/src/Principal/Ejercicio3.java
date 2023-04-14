b4ee5265ad2abcbb4f3defbe6df3def8144854b6
AlejoxVargas <henrryalejandro21@gmail.com>
2023-04-14 13:50:35 +0200

    7.1

--- EclipseWorkSpace/Retos7_1/src/Principal/Ejercicio3.java
+++ EclipseWorkSpace/Retos7_1/src/Principal/Ejercicio3.java
@@ -1,29 +1,48 @@
 package Principal;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Scanner;
 
-public class Ejercicio3 {
+public class Ejercicio3 implements FilenameFilter {
 
-	public static void main(String[] args) {
-		EjemploFile();
-	}
+    public static void main(String[] args) {
+        EjemploFile();
+    }
 
-	static Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
 
-	public static void EjemploFile() {
-		
-		System.out.println("Introduce la ruta de un fichero: ");
-		String ruta1 = sc.nextLine();
+    public static void EjemploFile() {
 
-		File file1 = new File(ruta1);
-		
-		if (file1.exists()) {
-			if (file1.canRead() && file1.canWrite()) {
-			
-			}
-		} else {
-			System.err.println("El fichero no existe.");
-		}
-	}
+        System.out.println("Introduce la ruta de un fichero: ");
+        String ruta1 = sc.nextLine();
+
+        File file1 = new File(ruta1);
+
+        if (file1.exists()) {
+
+            File[] directorios = file1.listFiles();
+            String[] filtrar = file1.list(new Ejercicio3('g'));
+            int sizeTotal = 0;
+
+            for (File f : directorios) {
+                System.out.println(f.getAbsolutePath());
+                sizeTotal += directorios.length;
+            }
+            System.out.println("Tamaño total: " + sizeTotal);
+        } else {
+            System.err.println("El fichero no existe.");
+        }
+    }
+
+    char letra;
+
+    @Override
+    public boolean accept(File dir, String name) {
+        return name.charAt();
+    }
+
+    Ejercicio3(char letra) {
+        this.letra = letra;
+    }
 }
